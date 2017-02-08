2fd76d88bfce4e9ec21dcee5769bbf28deec8c05
Anton Reitarovskiy <reitarovskii.toh@gmail.com>
2017-02-08 14:16:04 +0200

    Add user free memory to template

---
+++ app/context_processors/user_memory.py
@@ -0,0 +1,20 @@
+from django.template.context_processors import request
+import os
+
+def dir_size(path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+def memory(request):
+    if (not request.user):
+        return {}
+    else:
+        dir = os.path.join('app/media', request.user.username)
+        total_size = 52400000
+        used_size = dir_size(dir)
+        free_size = total_size - used_size
+        return {'total_size': total_size, 'used_size': used_size, 'free_size': free_size}
